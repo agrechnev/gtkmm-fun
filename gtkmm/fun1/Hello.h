6595116ca078f8cbfeef2e9939e420b5d7122a3c
Oleksiy Grechnyev <shrike4625@gmail.com>
2017-12-20 18:49:18 +0200

    radio buttons in progress

--- gtkmm/fun1/Hello.h
+++ gtkmm/fun1/Hello.h
@@ -5,19 +5,35 @@
 #pragma once
 
 #include <iostream>
-#include <gtkmm/image.h>
+#include <gtkmm/radiobutton.h>
 #include <gtkmm/window.h>
+#include <gtkmm/box.h>
+#include <gtkmm/separator.h>
 
 class Hello : public Gtk::Window {
 public:
     Hello() {
         set_border_width(10);
         set_title("Handmaiden's Love !!!");
-        add(img);
-        img.show();
+
+        rb2.join_group(rb1);
+        rb3.join_group(rb1);
+        box1.pack_start(rb1);
+        box1.pack_start(rb2);
+        box1.pack_start(rb3);
+        add(box1);
+
+        show_all_children();
     }
 
 protected:
-//    Gtk::Image img{"elves.jpg"};
-    Gtk::Image img{Gdk::Pixbuf::create_from_file("elves.jpg")};
+    Gtk::RadioButton rb1{"Nel"};
+    Gtk::RadioButton rb2{"Maria"};
+    Gtk::RadioButton rb3{"Sophia"};
+
+    Gtk::Button buClose{"Close"};
+
+    Gtk::Box box0{Gtk::ORIENTATION_VERTICAL};
+    Gtk::Box box1{Gtk::ORIENTATION_VERTICAL, 10};
+    Gtk::Box box2{Gtk::ORIENTATION_VERTICAL, 10};
 };
