e288c089ffeb13626d9c9c23e77b93858f1413c7
agrechnev <shrike4625@gmail.com>
2018-01-12 10:31:00 +0200

    Fun with Entry

--- gtkmm/fun2/Fun2Win.cpp
+++ gtkmm/fun2/Fun2Win.cpp
@@ -10,11 +10,21 @@ Fun2Win::Fun2Win() {
     set_title("FUN : Gtk::Entry");
 //    set_border_width(5);
 //    set_default_size(400, 100);
+    set_size_request(400, 100);
 
     // GUI
     add(boxV);
+
+    // Entry
+    ent.set_max_length(40);
+    ent.set_text("Goblins WON !!!");
     boxV.pack_start(ent, Gtk::PACK_SHRINK);
-    boxV.pack_start(lbl, Gtk::PACK_EXPAND_WIDGET);
+
+    ent.set_icon_from_icon_name("edit-find");
+    ent.signal_icon_press().connect([this](Gtk::EntryIconPosition /* icon_pos */,
+                                    const GdkEventButton* /* event */)->void{
+        std::cout << "Icon pressed !!!" << std::endl;
+    });
 
     // Close
     boxV.pack_start(btnClose);
@@ -25,74 +35,6 @@ Fun2Win::Fun2Win() {
     btnClose.grab_default();
     show_all_children();
 
-    // Model
-    Glib::RefPtr<Gtk::EntryCompletion> cmpl = Gtk::EntryCompletion::create();
-    ent.set_completion(cmpl);
-    auto cmplModel = Gtk::ListStore::create(mCols);
-    cmpl->set_model(cmplModel);
-
-    cmpl->set_match_func(sigc::mem_fun(*this, &Fun2Win::onMatch));
-
-    // Fill the model
-    Gtk::TreeModel::Row row;
-    row = *(cmplModel->append());
-    row[mCols.id] = 1;
-    row[mCols.name] = "Alan Zebedee";
-
-    row = *(cmplModel->append());
-    row[mCols.id] = 2;
-    row[mCols.name] = "Adrian Boo";
-
-    row = *(cmplModel->append());
-    row[mCols.id] = 3;
-    row[mCols.name] = "Bob McRoberts";
-
-    row = *(cmplModel->append());
-    row[mCols.id] = 4;
-    row[mCols.name] = "Bob McBob";
-
-    cmpl->set_text_column(mCols.name);
-
-    // Add extra choices, it seems
-    for (const auto & p: cmplActions) {
-        cmpl->insert_action_text(p.second, p.first);
-    }
-
-    cmpl->signal_action_activated().connect([](int i)->void{
-        std::cout << "signal_action_activated index = " << i << std::endl;
-    });
-
-    cmpl->signal_insert_prefix().connect([](const Glib::ustring& prefix)->bool{
-        std::cout << "signal_insert_prefix prefix = " << prefix << std::endl;
-        return false;
-    }, false);
-
-    cmpl->signal_match_selected().connect([this](const Gtk::TreeModel::iterator& iter)->bool{
-        const Gtk::TreeModel::Row & row = *iter;
-        std::cout << "signal_match_selected name = " << row[mCols.name] << std::endl;
-        return false;
-    }, false);
-
 
 }
 
-bool Fun2Win::onMatch(const Glib::ustring &key, const Gtk::TreeModel::const_iterator &iter){
-    std::cout << " onMatch()  " << key << std::endl;
-
-    if (iter) {
-        Gtk::TreeModel::Row row = *iter;
-        Glib::ustring::size_type keySize = key.size();
-        Glib::ustring filterStr = row[mCols.name];
-        Glib::ustring fSStart = filterStr.substr(0, keySize);
-        std::cout << filterStr << " " << fSStart << std::endl;
-        fSStart = fSStart.lowercase();
-
-        if (key == fSStart) {
-            std::cout << "TRUE !!!" << std::endl;
-            return true;
-        }
-    }
-    std::cout << "FALSE !!!" << std::endl;
-    return false; // No match
-
-}
--- gtkmm/fun2/Fun2Win.h
+++ gtkmm/fun2/Fun2Win.h
@@ -13,30 +13,8 @@ public:
     Fun2Win();
 
 protected:
-    // Tree model
-    struct ModelColumns : public Gtk::TreeModel::ColumnRecord{
-        ModelColumns(){
-            add(id);
-            add(name);
-        }
-
-        Gtk::TreeModelColumn<unsigned> id;
-        Gtk::TreeModelColumn<Glib::ustring> name;
-    };
-
-    using TypeActionMap = std::map<int, Glib::ustring>;
-
-    ModelColumns mCols;
-    TypeActionMap cmplActions{{0, "Use Wizard"}, {1, "Browse for filename"}};
-
-    // Gui
     Gtk::Box boxV{Gtk::ORIENTATION_VERTICAL};
-
     Gtk::Entry ent;
-    Gtk::Label lbl;
-
     Gtk::Button btnClose{"Close"};
 
-protected: //========Methods
-    bool onMatch(const Glib::ustring & key, const Gtk::TreeModel::const_iterator & iter);
 };
